a12249823c4b7e83ffe8dbb94dd12a1386d65c66
Keating Reid <keating950@gmail.com>
2019-11-30 16:03:52 -0500

    changing OOP approach to static methods

--- gitsearch.py
+++ gitsearch.py
@@ -1,33 +1,23 @@
-from ResultFetcher import ResultFetcher
+import ResultFetcher
 import sys
 import curses
 import os
-
-
-def terminate_curses(stdscr):
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
 
 
 def main(stdscr):
     arg_str = ""
     for i in sys.argv:
         arg_str += str(i)
-    result_fetcher = ResultFetcher()
-    results = result_fetcher.fetch_results()
+    results = ResultFetcher.fetch_results()
 
     rows, cols = tuple(map(
         lambda x: int(x),
         os.popen('stty size', 'r').read().split()
     ))
 
-    results_pad = curses.newpad(len(results) * 3, cols)
-
-    sidebar = curses.newwin(rows, cols // 4, 0, 0)
-    stdscr.refresh()
-    sidebar.refresh()
+    results_pad = curses.newpad(len(results) * 3, rows)
+    # results_pad.refresh(0, 0, 0, 0, rows, cols)
+    # stdscr.refresh()
 
 
 if __name__ == "__main__":
