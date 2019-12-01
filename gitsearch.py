aa4b0342b312cd6867f322379016aeb50363d48d
Keating Reid <keating950@gmail.com>
2019-12-01 12:47:49 -0500

    limiting cursor position

--- gitsearch.py
+++ gitsearch.py
@@ -20,18 +20,18 @@ def format_results(results: list) -> list:
 
 
 def init_pad(contents):
-    pad = curses.newpad(len(contents)+1, curses.COLS)
+    pad = curses.newpad(len(contents) + 1, curses.COLS)
     line_position = 0
     for idx, line in enumerate(contents):
         if line is not None:
-            if idx%4==0:
+            if idx % 4 == 0:
                 pad.addnstr(line_position, 0, line, curses.COLS - 1, curses.A_BOLD)
             else:
-                pad.addnstr(line_position, 0, line, curses.COLS-1)
+                pad.addnstr(line_position, 0, line, curses.COLS - 1)
         else:
-            pad.addnstr(line_position, 0, "\n", curses.COLS-1)
-        line_position+=1
-    pad.refresh(0, 0, 0, 0, curses.LINES-1, curses.COLS-1)
+            pad.addnstr(line_position, 0, "\n", curses.COLS - 1)
+        line_position += 1
+    pad.refresh(0, 0, 0, 0, curses.LINES - 1, curses.COLS - 1)
     return pad
 
 
@@ -45,16 +45,42 @@ def main(stdscr):
 
     results_pad = init_pad(line_list)
     pad_pos = 0
+    curs_y, curs_x = curses.getsyx()
     stdscr.refresh()
 
     while True:
         c = stdscr.getch()
         if c == ord("j"):
-            pad_pos+=1
-        if c == ord("k"):
-            pad_pos-=1
-        elif c == curses.KEY_DOWN:
-            pass
+            pad_pos += 1
+        elif c == ord("k"):
+            pad_pos -= 1
+
+        elif c in {curses.KEY_DOWN, curses.KEY_UP}:
+            y, x = stdscr.getyx()
+            if c == curses.KEY_DOWN:
+                if y + 1 >= curses.LINES:
+                    pad_pos += 1
+                else:
+                    stdscr.move(y + 1, x)
+            elif c == curses.KEY_UP:
+                if y - 1 < 0:
+                    pad_pos -= 1
+                else:
+                    stdscr.move(y - 1, x)
+
+        elif c in {curses.KEY_LEFT, curses.KEY_RIGHT}:
+            y, x = stdscr.getyx()
+            if c == curses.KEY_LEFT:
+                if x - 1 < 0:
+                    pass
+                else:
+                    stdscr.move(y, x - 1)
+            elif c == curses.KEY_RIGHT:
+                if x + 1 > curses.COLS - 1:
+                    pass
+                else:
+                    stdscr.move(y, x + 1)
+
         results_pad.refresh(pad_pos, 0, 0, 0, curses.LINES - 1, curses.COLS - 1)
 
 
