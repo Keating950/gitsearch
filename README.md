c24457e82fe0906c8f404e455c981d14a9f45ec8
Keating Reid <keating950@gmail.com>
2019-12-06 16:28:03 -0500

    major refactoring of input_stream() to be more modular

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gitsearch
 Simple command line utility for searching Github.
+
+Move the cursor with vim-style hjkl inputs.
+Scroll with the keypad.
--- gitsearch.py
+++ gitsearch.py
@@ -3,6 +3,8 @@ import ResultFetcher
 import argparse
 import sys
 import curses
+from curses import textpad
+import re
 
 
 def parse_args() -> argparse.Namespace:
@@ -22,11 +24,11 @@ def parse_args() -> argparse.Namespace:
     return arg_namespace
 
 
-def make_entry_objects(results: list) -> list:
+def gen_entry_objects(results: list) -> list:
     entries = []
     for repo in results:
         entry = Entry(repo['name'], repo['owner']['login'], int(repo['stargazers_count']),
-                      repo['url'], repo.get('language'), repo.get('description'),
+                      repo['html_url'], repo.get('language'), repo.get('description'),
                       )
         entries.append(entry)
     return entries
@@ -47,50 +49,71 @@ def init_pad(contents):
 
 def input_stream(pad, pad_max_y: int):
     pad_pos = 0
-    curs_y, curs_x = curses.getsyx()
     stdscr.refresh()
+
+    def mv_cursor_and_scroll(pad_pos, c):
+        if c == curses.KEY_DOWN:
+            pad_pos += 1
+        elif c == curses.KEY_UP:
+            pad_pos -= 1
+
+        y, x = curses.getsyx()
+        if c == ord("j"):
+            if y == curses.LINES - 1:
+                pad_pos += 1
+                return pad_pos
+            else:
+                curses.setsyx(y + 1, x)
+        elif c == ord("k"):
+            if y == 0:
+                if pad_pos > 0:
+                    pad_pos -= 1
+                else:
+                    return pad_pos
+            else:
+                curses.setsyx(y - 1, x)
+
+        elif c in {curses.KEY_LEFT, curses.KEY_RIGHT, ord("h"), ord("l")}:
+            y, x = curses.getsyx()
+            if c in {curses.KEY_LEFT, ord("h")}:
+                if x - 1 < 0:
+                    return pad_pos
+                else:
+                    curses.setsyx(y, x - 1)
+            elif c in {curses.KEY_RIGHT, ord("l")}:
+                if x + 1 > curses.COLS - 1:
+                    return pad_pos
+                else:
+                    curses.setsyx(y, x + 1)
+        return pad_pos
+
+    def url_popup(url):
+        if re.match(url,
+                    "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,"
+                    "6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"):
+            textpad.rectangle(stdscr, (uly := curses.LINES // 8),
+                              (ulx := curses.COLS // 8),
+                              uly * 2, ulx * 2)
+
+        return
 
     while True:
         c = stdscr.getch()
 
-        if c == ord("j"):
-            pad_pos += 1
-        elif c == ord("k"):
-            pad_pos -= 1
+        if c in {ord("h"), ord("j"), ord("k"), ord("l"),
+                 curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT}:
+            pad_pos = mv_cursor_and_scroll(pad_pos, c)
+            if pad_pos < pad_max_y - curses.LINES:
+                pad.refresh(pad_pos, 0, 0, 0, curses.LINES - 1, curses.COLS - 1)
+            else:
+                pad_pos = (pad_max_y - curses.LINES) - 1
+            continue
 
-        elif c in {curses.KEY_DOWN, curses.KEY_UP}:
+        elif c == curses.KEY_ENTER:
             y, x = curses.getsyx()
-            if c == curses.KEY_DOWN:
-                if y == curses.LINES - 1:
-                    pad_pos += 1
-                else:
-                    curses.setsyx(y + 1, x)
-            elif c == curses.KEY_UP:
-                if y == 0:
-                    if pad_pos > 0:
-                        pad_pos -= 1
-                    else:
-                        pass
-                else:
-                    curses.setsyx(y - 1, x)
-
-        elif c in {curses.KEY_LEFT, curses.KEY_RIGHT}:
-            y, x = curses.getsyx()
-            if c == curses.KEY_LEFT:
-                if x - 1 < 0:
-                    pass
-                else:
-                    curses.setsyx(y, x - 1)
-            elif c == curses.KEY_RIGHT:
-                if x + 1 > curses.COLS - 1:
-                    pass
-                else:
-                    curses.setsyx(y, x + 1)
-
-        if pad_pos < pad_max_y-curses.LINES:
-            pad.refresh(pad_pos, 0, 0, 0, curses.LINES - 1, curses.COLS - 1)
-        else:
-            pad_pos = (pad_max_y-curses.LINES)-1
+            url = pad.instr(y, 0)
+            url_popup(url)
+            stdscr.refresh()
 
 
 def main(stdscr):
@@ -100,12 +123,18 @@ def main(stdscr):
 
     formatted_args = parse_args()
     results = ResultFetcher.fetch_results(formatted_args)
-    entry_list = make_entry_objects(results)
+    entry_list = gen_entry_objects(results)
 
     results_pad = init_pad(entry_list)
     results_pad.refresh(0, 0, 0, 0, curses.LINES - 1, curses.COLS - 1)
     pad_max_y = sum([entry.numlines for entry in entry_list]) + len(entry_list) + 1
-    input_stream(results_pad, pad_max_y)
+
+    try:
+        input_stream(results_pad, pad_max_y)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        curses.endwin()
 
 
 if __name__ == "__main__":
