3fffa264ee9419962ffae7b5ce744497cb4db5a0
Keating Reid <keating950@gmail.com>
2020-02-26 11:17:07 -0500

    folding RepoCloner into main

--- gitsearch.py
+++ gitsearch.py
@@ -4,13 +4,16 @@ from MainWindow import MainWindow
 import FetchResults
 import argparse
 import curses
-import RepoCloner
+import os
+import subprocess
+from typing import Union
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="A command line program to search for"
+                                                 "and clone repos from GitHub.")
     parser.add_argument("--query", metavar="query string", type=str, nargs=1,
-                        help="A quoted query string")
+                        help="A quoted query string", required=True)
     parser.add_argument("--sort", metavar="sort by", type=str, nargs=1,
                         help="sort by stars, forks, help-wanted-issues, "
                              "or updated."
@@ -20,17 +23,38 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--lang", metavar="lang", type=str, nargs=1,
                         help="Restrict results by language.")
     arg_namespace = parser.parse_args()
-    # splitting multi-word double-quoted query arguments
+    # splitting multi-word double-quoted query arguments for proper url formatting
     arg_namespace.query = str(arg_namespace.query[0]).split()
     return arg_namespace
 
 
+def format_validate_path(path: str) -> Union[Exception, str]:
+    abs_path = os.path.expanduser(path).strip()
+    if not os.path.isdir(abs_path):
+        raise FileNotFoundError
+    return abs_path
+
+
+def clone_repo(path: str, url: str) -> None:
+    og_cwd = os.getcwd()
+    try:
+        os.chdir(path)
+        subprocess.check_call(["git", "clone", url],
+                              stderr=subprocess.DEVNULL,
+                              stdout=subprocess.DEVNULL
+                              )
+    except Exception as e:
+        curses.endwin()
+        raise e
+    os.chdir(og_cwd)
+
+
 if __name__ == "__main__":
+    formatted_args = parse_args()
     stdscr = curses.initscr()
     curses.noecho()
     curses.cbreak()
     stdscr.keypad(True)
-    formatted_args = parse_args()
     entries = FetchResults.gen_entries(
         FetchResults.fetch(formatted_args)
     )
@@ -42,11 +66,11 @@ if __name__ == "__main__":
         while True:
             path, url = main_window.input_stream()
             try:
-                path_f = RepoCloner.format_validate_path(path)
+                path_f = format_validate_path(path)
             except FileNotFoundError:
                 main_window.draw_path_error_window(path)
                 continue
-            RepoCloner.clone_repo(path_f, url)
+            clone_repo(path_f, url)
             main_window.clear_popup_win(url.split("/")[-1], path_f)
     except KeyboardInterrupt:
         pass
